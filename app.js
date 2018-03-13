4690104d1ed974cd87d07fd403d76a0438dff500
Allen Wang <sixesrai3@gmail.com>
2018-03-13 09:59:39 +0800

    hello world

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
